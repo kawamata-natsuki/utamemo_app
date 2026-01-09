9cb2be30dd9b69590fa5315001777de0c6ae7462
KAWA <t.natsuki020817@gmail.com>
2026-01-09 13:29:06 +0900

    style: コンストラクタの key を super parameter に変更

--- lib/presentation/screens/s10_song_list/s10_song_list_page.dart
+++ lib/presentation/screens/s10_song_list/s10_song_list_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:utamemo_app/constants/colors.dart';
 
 class SongsListScreen extends StatelessWidget {
-  const SongsListScreen({Key? key}) : super(key: key);
+  const S10SongListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
